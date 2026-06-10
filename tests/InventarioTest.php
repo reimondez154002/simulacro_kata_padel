ef0b30e2c696b52e908134457619de55983b8add
reimondez154002 <reimondez.154002@e.unavarra.es>
2026-06-10 18:28:32 +0200

    [refactor] - Extraer Mock a setUp()

--- tests/InventarioTest.php
+++ tests/InventarioTest.php
@@ -6,221 +6,111 @@ require_once __DIR__ . "/../src/Inventario.php";
 
 class InventarioTest extends TestCase{
 
-    public function test_vaciar_devuelve_respuesta_de_vaciado_correcto():void{
-        //Preparar(Preparar Mock)
+    private Inventario $inventario;
+    public function setUp():void{
+         //Preparar(MOCK)
         $catalogoMock = $this->createMock(Catalogo::class);
-        $inventario = new Inventario($catalogoMock);
+        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
+            if($articulo === "raqueta"){
 
+                return 50.00;
+            }
+
+            if($articulo === "pelotas"){
+
+                return 5.00;
+            }
+        });
+        $this->inventario = new Inventario($catalogoMock);
+    }
+    
+    public function test_vaciar_devuelve_respuesta_de_vaciado_correcto():void{
         //Ejecutar Accion
-        $resultado = $inventario->ejecutar("vaciar");
+        $resultado = $this->inventario->ejecutar("vaciar");
 
         //Comprobar
         $this->assertEquals("El inventario ha sido vaciado", $resultado);
     }
 
     public function test_funcion_cuenta_vacia_devuelve_00():void{
-        //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $resultado = $inventario->ejecutar("cuenta");
+        $resultado = $this->inventario->ejecutar("cuenta");
 
         //Comprobar
         $this->assertEquals("Total: 0.00", $resultado);
     }
 
     public function test_funcion_añadir_un_artiulo_sin_cantidad_a_inventario():void{
-        //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $resultado = $inventario->ejecutar("añadir raqueta");
+        $resultado = $this->inventario->ejecutar("añadir raqueta");
 
         //Comprobar
         $this->assertEquals("raqueta x1", $resultado);
     }
 
     public function test_funcion_añadir_varios_articulos_los_devuelve_ordenados_alfabeticamente():void{
-        //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-            
-            if($articulo === "pelotas"){
-
-                return 5.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $resultado = $inventario->ejecutar("añadir raqueta");
-        $resultado = $inventario->ejecutar("añadir pelotas");
+        $resultado = $this->inventario->ejecutar("añadir raqueta");
+        $resultado = $this->inventario->ejecutar("añadir pelotas");
 
         //Comprobar
         $this->assertEquals("pelotas x1, raqueta x1", $resultado);
     }
 
         public function test_añadir_articulo_que_ya_existe():void{
-                //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-            
-            if($articulo === "pelotas"){
-
-                return 5.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $resultado = $inventario->ejecutar("añadir raqueta");
-        $resultado = $inventario->ejecutar("añadir raqueta");
+        $resultado = $this->inventario->ejecutar("añadir raqueta");
+        $resultado = $this->inventario->ejecutar("añadir raqueta");
 
         //Comprobar
         $this->assertEquals("raqueta x2", $resultado);
     }
     public function test_añadir_articulo_con_cantidad():void{
-                //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-            
-            if($articulo === "pelotas"){
-
-                return 5.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $resultado = $inventario->ejecutar("añadir raqueta 2");
+        $resultado = $this->inventario->ejecutar("añadir raqueta 2");
         //Comprobar
         $this->assertEquals("raqueta x2", $resultado);
     }
 
     public function test_añadir_articulo_que_no_existe_en_el_catalogo():void{
-                //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-            
-            if($articulo === "pelotas"){
-
-                return 5.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $resultado = $inventario->ejecutar("añadir patatas");
+        $resultado = $this->inventario->ejecutar("añadir patatas");
         //Comprobar
         $this->assertEquals("El articulo no existe en el catalogo", $resultado);
     }
     
     public function test_vaciar_inventario_no_vacio():void{
-                //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-            
-            if($articulo === "pelotas"){
-
-                return 5.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $inventario->ejecutar("añadir patatas");
-        $resultado = $inventario->ejecutar("vaciar");
+        $this->inventario->ejecutar("añadir patatas");
+        $resultado = $this->inventario->ejecutar("vaciar");
         //Comprobar
         $this->assertEquals("El inventario ha sido vaciado", $resultado);
     }
 
     public function test_eliminar_elemento_de_inventario_lo_borra():void{
-        //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-            
-            if($articulo === "pelotas"){
-
-                return 5.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $inventario->ejecutar("añadir pelotas");
-        $inventario->ejecutar("añadir raqueta");
-        $resultado = $inventario->ejecutar("eliminar raqueta");
+        $this->inventario->ejecutar("añadir pelotas");
+        $this->inventario->ejecutar("añadir raqueta");
+        $resultado = $this->inventario->ejecutar("eliminar raqueta");
         //Comprobar
         $this->assertEquals("pelotas x1", $resultado);
     }
 
     public function test_eliminar_elemento_no_existente():void{
-        //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-            
-            if($articulo === "pelotas"){
-
-                return 5.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $inventario->ejecutar("añadir pelotas");
-        $inventario->ejecutar("añadir raqueta");
-        $resultado = $inventario->ejecutar("eliminar agua");
+        $this->inventario->ejecutar("añadir pelotas");
+        $this->inventario->ejecutar("añadir raqueta");
+        $resultado = $this->inventario->ejecutar("eliminar agua");
         //Comprobar
         $this->assertEquals("El elemento a eliminar no esta en el inventario", $resultado);
     }
 
     public function test_eliminar_hasta_vaciar():void{
-        //Preparar(MOCK)
-        $catalogoMock = $this->createMock(Catalogo::class);
-        $catalogoMock->method("getPrecio")->willReturnCallback(function($articulo){
-            if($articulo === "raqueta"){
-
-                return 50.00;
-            }
-            
-            if($articulo === "pelotas"){
-
-                return 5.00;
-            }
-        });
-        $inventario = new Inventario($catalogoMock);
         //Ejecutar Accion
-        $inventario->ejecutar("añadir pelotas");
-        $inventario->ejecutar("añadir raqueta");
-        $inventario->ejecutar("eliminar raqueta");
-        $resultado = $inventario->ejecutar("eliminar pelotas");
+        $this->inventario->ejecutar("añadir pelotas");
+        $this->inventario->ejecutar("añadir raqueta");
+        $this->inventario->ejecutar("eliminar raqueta");
+        $resultado = $this->inventario->ejecutar("eliminar pelotas");
 
         //Comprobar
         $this->assertEquals("La lista ha sido vaciada", $resultado);
